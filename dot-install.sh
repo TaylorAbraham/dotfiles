8b109d962eb0a67126a2832f8214ac37cb91052c
Logan <Ryan_JA@live.ca>
2017-09-13 18:44:50 -0400

    Added comments to install script

--- dot-install.sh
+++ dot-install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+# Dotfile paths
 dir=~/dotfiles
 backupdir=~/dotfiles_old
 dotfiles=".zshrc .tmux.conf .vimrc"
 
+# Make backup folder
 echo "Creating $olddir for current dotfiles backup..."
 mkdir -p $backupdir
 echo "Done"
 
-echo "Changing to dotfiles directory..."
+# Back up each dotfile and symlink to the new ones
 cd $dir
-echo "Done"
-
 for dotfile in $dotfiles; do
   echo "Backing up old $dotfile..."
   mv ~/$dotfile $backupdir
@@ -21,4 +21,5 @@ for dotfile in $dotfiles; do
   echo "Done"
 done
 
+# Restart zsh
 source ~/.zshrc
